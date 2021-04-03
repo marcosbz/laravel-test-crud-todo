f83794c6db11b49678e41aebbe4c2e4dca33ba80
marcosbz <ziegler.marcos.b@gmail.com>
2021-04-03 19:20:13 -0300

    Add Todo table fields in migration

--- database/migrations/2021_04_03_221415_create_todos_table.php
+++ database/migrations/2021_04_03_221415_create_todos_table.php
@@ -16,6 +16,9 @@ class CreateTodosTable extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('completed');
         });
     }
 
